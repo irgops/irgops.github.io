e00d0f189d974ed3003f299a67ee2a79a2389898
irg-shaun <shaun.carroll@integratedresponsegroup.com>
2026-03-06 11:49:01 -0500

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'static',
+  site: 'https://integratedresponsegroup.com',
+  base: '/',
+  // base: '/integrated-response-group/',
+  vite: {
+    plugins: [tailwindcss(), svgr()],
+  },
+});
